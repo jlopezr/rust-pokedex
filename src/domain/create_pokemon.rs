fc8975d9bad22176961ffea57419e69899223bf9
Juan López Rubio <j.lopez.r@gmail.com>
2022-02-23 17:43:52 +0100

    Domain working

--- src/domain/create_pokemon.rs
+++ src/domain/create_pokemon.rs
@@ -1,4 +1,5 @@
 use crate::domain::entities::{PokemonName, PokemonNumber, PokemonTypes};
+use crate::repositories::pokemon::{Repository, Insert, InMemoryRepository};
 
 struct Request {
     number: u16,
@@ -11,15 +12,21 @@ struct Request {
 enum Response {
     Ok(u16),
     BadRequest,
+    Conflict,
+    Error
 }
 
-fn execute(req: Request) -> Response {
+fn execute(repo: &mut dyn Repository, req: Request) -> Response {
     match (
         PokemonNumber::try_from(req.number),
         PokemonName::try_from(req.name),
         PokemonTypes::try_from(req.types),
     ) {
-        (Ok(number), Ok(_), Ok(_)) => Response::Ok(u16::from(number)),
+        (Ok(number), Ok(names), Ok(types)) => match repo.insert(number, names, types) {
+            Insert::Ok(number) => Response::Ok(u16::from(number)),
+            Insert::Conflict => Response::Conflict,
+            Insert::Error => Response::Error,
+        },
         _ => Response::BadRequest,
     }
 }
@@ -30,6 +37,7 @@ mod tests {
 
     #[test]
     fn it_should_return_the_pokemon_number_otherwise() {
+        let mut repo = InMemoryRepository::new();
         let number = 25;
         let req = Request {
             number,
@@ -37,7 +45,7 @@ mod tests {
             types: vec![String::from("Electric")],
         };
 
-        let res = execute(req);
+        let res = execute(&mut repo, req);
 
         match res {
             Response::Ok(res_number) => assert_eq!(res_number, number),
@@ -47,17 +55,57 @@ mod tests {
     
     #[test]
     fn it_should_return_a_bad_request_error_when_request_is_invalid() {
+        let mut repo = InMemoryRepository::new();
         let req = Request {
             number: 25,
             name: String::from(""),
             types: vec![String::from("Electric")],
         };
 
-        let res = execute(req);
+        let res = execute(&mut repo, req);
 
         match res {
             Response::BadRequest => {}
             _ => unreachable!(),
         };
-    }     
+    }
+
+    #[test]
+    fn it_should_return_a_conflict_error_when_pokemon_number_already_exists() {
+        let number = PokemonNumber::try_from(25).unwrap();
+        let name = PokemonName::try_from(String::from("Pikachu")).unwrap();
+        let types = PokemonTypes::try_from(vec![String::from("Electric")]).unwrap();
+        let mut repo = InMemoryRepository::new();
+        repo.insert(number, name, types);
+        let req = Request {
+            number: 25,
+            name: String::from("Charmander"),
+            types: vec![String::from("Fire")],
+        };
+
+        let res = execute(&mut repo, req);
+
+        match res {
+            Response::Conflict => {}
+            _ => unreachable!(),
+        }
+    }
+
+    #[test]
+    fn it_should_return_an_error_when_an_unexpected_error_happens() {
+        let mut repo = InMemoryRepository::new().with_error();
+        let number = 25;
+        let req = Request {
+            number,
+            name: String::from("Pikachu"),
+            types: vec![String::from("Electric")],
+        };
+
+        let res = execute(&mut repo, req);
+
+        match res {
+            Response::Error => {},
+            _ => unreachable!(),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+mod api;
 mod domain;
+mod repositories;
 
+#[macro_use]
+extern crate rouille;
 fn main() {
     println!("Hello, world!");
 }
