e8391dbe90a0e6b36db488de7d7ee4e47312be2c
Juan López Rubio <j.lopez.r@gmail.com>
2022-02-24 16:26:26 +0100

    first option in cli

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 use repositories::pokemon::InMemoryRepository;
 use std::sync::Arc;
+use clap::{Arg, Command};
+
 
 mod api;
 mod domain;
 mod repositories;
+mod cli;
 
 #[macro_use]
 extern crate rouille;
 
 extern crate serde;
 
+#[macro_use]
+extern crate clap;
+
 fn main() {
     let repo = Arc::new(InMemoryRepository::new());
-    api::serve("localhost:8000", repo);
+    
+    let matches = Command::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .arg(Arg::new("cli").long("cli").help("Runs in CLI mode"))
+        .get_matches();
+
+    match matches.occurrences_of("cli") {
+        0 => api::serve("localhost:8000", repo),
+        _ => cli::run(repo),
+    }        
 }
